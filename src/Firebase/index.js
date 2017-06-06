f88d3dda62d56caaa0232d8879d27effa845ba1e
jean4599@ <jean4599@gmail.com>
2017-06-07 00:16:06 +0900

    video 2

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyANtxGKGUOri82WTBtkEEWf0lmFSUwx1eM",
-    authDomain: "vision-2-d2ab1.firebaseapp.com",
-    databaseURL: "https://vision-2-d2ab1.firebaseio.com",
-    projectId: "vision-2-d2ab1",
-    storageBucket: "vision-2-d2ab1.appspot.com",
-    messagingSenderId: "903144066527"
+    apiKey: "AIzaSyC7b_klqroPHafD1gpSZdzkkVyYSNW89d4",
+    authDomain: "vision-57ab7.firebaseapp.com",
+    databaseURL: "https://vision-57ab7.firebaseio.com",
+    projectId: "vision-57ab7",
+    storageBucket: "vision-57ab7.appspot.com",
+    messagingSenderId: "574746063767"
   };
   firebase.initializeApp(config);
 
