86b34e61884660cf8c92993748cd4178d1011305
jean4599@ <jean4599@gmail.com>
2017-06-11 23:41:29 +0900

    rating UI and js changed

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,31 +1,31 @@
 import firebase from 'firebase'  
 
  // Initialize Firebase
-  //AWG
-  var config = {
-    apiKey: "AIzaSyC7b_klqroPHafD1gpSZdzkkVyYSNW89d4",
-    authDomain: "vision-57ab7.firebaseapp.com",
-    databaseURL: "https://vision-57ab7.firebaseio.com",
-    projectId: "vision-57ab7",
-    storageBucket: "vision-57ab7.appspot.com",
-    messagingSenderId: "574746063767"
-  };
-  //persuation
+  // //AWG
   // var config = {
-  //   apiKey: "AIzaSyANtxGKGUOri82WTBtkEEWf0lmFSUwx1eM",
-  //   authDomain: "vision-2-d2ab1.firebaseapp.com",
-  //   databaseURL: "https://vision-2-d2ab1.firebaseio.com",
-  //   projectId: "vision-2-d2ab1",
-  //   storageBucket: "vision-2-d2ab1.appspot.com",
-  //   messagingSenderId: "903144066527"
+  //   apiKey: "AIzaSyC7b_klqroPHafD1gpSZdzkkVyYSNW89d4",
+  //   authDomain: "vision-57ab7.firebaseapp.com",
+  //   databaseURL: "https://vision-57ab7.firebaseio.com",
+  //   projectId: "vision-57ab7",
+  //   storageBucket: "vision-57ab7.appspot.com",
+  //   messagingSenderId: "574746063767"
   // };
+  //persuation
+  var config = {
+    apiKey: "AIzaSyANtxGKGUOri82WTBtkEEWf0lmFSUwx1eM",
+    authDomain: "vision-2-d2ab1.firebaseapp.com",
+    databaseURL: "https://vision-2-d2ab1.firebaseio.com",
+    projectId: "vision-2-d2ab1",
+    storageBucket: "vision-2-d2ab1.appspot.com",
+    messagingSenderId: "903144066527"
+  };
   firebase.initializeApp(config);
 
 export default firebase
 
 export const REF = {
-  Node: '/Ryan/nodes',
-  Link: '/Ryan/edges',
+  Node: '/Shun-Huai Yao/nodes',
+  Link: '/Shun-Huai Yao/edges',
   Video: '/video',
 }
 
