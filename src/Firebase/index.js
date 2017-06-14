26fb9a8d8513bb4bca83f92f0b5a52625bd47a0c
jean4599@ <jean4599@gmail.com>
2017-06-15 02:38:54 +0900

    bug

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -15,8 +15,8 @@ import firebase from 'firebase'
 export default firebase
 
 export const REF = {
-  Node: '/result/Jane/nodes',
-  Link: '/result/Jane/edges',
+  Node: '/Data/nodes',
+  Link: '/Data/edges',
   Video: '/video',
 }
 
